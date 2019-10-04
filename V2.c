02106174446168536a0f7abd421f6de0ab4a9985
Joe <josephmorritt@gmail.com>
2019-10-04 18:44:23 +0100

    code add

---
+++ V2.c
@@ -0,0 +1,119 @@
+
+#pragma config FEXTOSC = OFF    // External Oscillator mode Selection bits (Oscillator not enabled)
+#pragma config RSTOSC = HFINTOSC_64MHZ// Power-up default value for COSC bits (HFINTOSC with HFFRQ = 64 MHz and CDIV = 1:1)
+
+
+// CONFIG1H
+#pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function is disabled)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+
+// CONFIG2L
+#pragma config MCLRE = EXTMCLR  // Master Clear Enable bit (If LVP = 0, MCLR pin is MCLR; If LVP = 1, RE3 pin function is MCLR )
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (Power up timer disabled)
+#pragma config LPBOREN = OFF    // Low-power BOR enable bit (ULPBOR disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled , SBOREN bit is ignored)
+
+// CONFIG2H
+#pragma config BORV = VBOR_2P45 // Brown Out Reset Voltage selection bits (Brown-out Reset Voltage (VBOR) set to 2.45V)
+#pragma config ZCD = OFF        // ZCD Disable bit (ZCD disabled. ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config DEBUG = OFF      // Debugger Enable bit (Background debugger disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Extended Instruction Set and Indexed Addressing Mode disabled)
+
+// CONFIG3L
+#pragma config WDTCPS = WDTCPS_31// WDT Period Select bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE = OFF       // WDT operating mode (WDT Disabled)
+
+// CONFIG3H
+#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+
+// CONFIG4L
+#pragma config WRT0 = OFF       // Write Protection Block 0 (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection Block 1 (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection Block 2 (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection Block 3 (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG4H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-30000Bh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+#pragma config SCANE = ON       // Scanner Enable bit (Scanner module is available for use, SCANMD bit can control the module)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored)
+
+// CONFIG5L
+#pragma config CP = OFF         // UserNVM Program Memory Code Protection bit (UserNVM code protection disabled)
+#pragma config CPD = OFF        // DataNVM Memory Code Protection bit (DataNVM code protection disabled)
+
+// CONFIG5H
+
+// CONFIG6L
+#pragma config EBTR0 = OFF      // Table Read Protection Block 0 (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection Block 1 (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection Block 2 (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection Block 3 (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG6H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0007FFh) not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#include <stdio.h>
+#include "lcd.h"
+
+int resultHigh = 0;
+int resultLow = 0;
+int Light = 0;
+char LCDtext[16];
+int Rthr = -1;
+int i = 0;
+int Rmax = 1023;
+int LON[]= {0x00,0x01,0x03,0x07,0x0f,0x1f,0x3f,0x7f,0xff};// could convert to decimal if wanted 
+
+
+int main(void) {
+
+    // Pin init
+    TRISC = 0;
+    TRISD = 0;
+  
+    TRISA = 0xFF;
+    
+    ANSELC = 0;
+    ANSELD = 0;
+    ANSELA = 0xFF;
+
+    LATC = 0;
+    LATD = 0;
+    LATE = 0; // Start with all pins driven LOW
+
+    OSCCON1bits.NDIV = 0;
+    ADCON0bits.ADFM = 1; //right justify
+    ADCON0bits.ADCS = 0; //FRC Clock
+
+    ADCLK = 4;
+    ADPCH = 0x00; //RA0 is Analog channel
+
+    TRISAbits.TRISA0 = 1; //Set RA0 to input
+
+    ANSELAbits.ANSELA0 = 1; //Set RA0 to analog
+
+    ADCON0bits.ADON = 1; //Turn ADC On
+
+    while (1) {
+            //ADPCH = 0x00; //RA0 is Analog channel
+            ADCON0bits.ADGO = 1; //Start conversion
+            while (ADCON0bits.GO); //Wait for conversion done
+            resultHigh = ADRESH; //Read result
+            resultLow = ADRESL; //Read result
+            Light = ((resultHigh << 8 | resultLow))/128;
+
+            PORTC = LON[Light];
+          
+    }
+       
+}
